a78131aa07a5ad10bd8119e09c94e36a12755b24
Amarjit <=>
2024-11-11 08:38:22 +0530

    handled build error

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -15,7 +15,7 @@ const ContactSection = () => {
                 Let&apos;s create something amazing together
               </h2>
               <p className="text-sm mt-2 md:text-base">
-                Ready to bring your next project to life? Let's connect and
+                Ready to bring your next project to life? Let&apos;s connect and
                 discuss how I can help you achieve your goals.
               </p>
             </div>
